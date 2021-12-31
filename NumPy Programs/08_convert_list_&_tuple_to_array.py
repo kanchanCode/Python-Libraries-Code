2b2986eeae3d39e52b2013a7ed4d957615f900b3
kanchanCode <jkanchan2911@gmail.com>
2021-12-31 09:40:52 +0530

    [Kanchan]Add:convert ist and tuple to array

--- NumPy Programs/08_convert_list_&_tuple_to_array.py
+++ NumPy Programs/08_convert_list_&_tuple_to_array.py
@@ -5,3 +5,20 @@
 @Last Modified time: 2021-12-30
 @Title :Write a list and tuple into arrays.
 '''
+import numpy as np
+
+list1=[1,2,3,4,5,6]
+print(type(list1))
+print("List is",list1)
+tuple1=(7,8,9,10,11,12)
+print(type(tuple1))
+print("Tuple is",tuple1)
+
+print("List to array is ",np.asarray(list1))
+#asarray=Convert the input to an array
+#returns->ndarray
+print(type(np.asarray(list1)))
+
+print("Tuple to array is ",np.asarray(tuple1))
+print(type(np.asarray(tuple1)))
+
