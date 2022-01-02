8bb7e195d3f969dc09eb7319a61a80f9064c5067
kanchanCode <jkanchan2911@gmail.com>
2022-01-02 08:49:57 +0530

    [Kanchan]Add:concatenate two arrays

--- NumPy Programs/21_cocatenate_2_arrays.py
+++ NumPy Programs/21_cocatenate_2_arrays.py
@@ -11,10 +11,16 @@ import numpy as np
 # import numpy
 
 # Creating two 2D arrays
-arr1 = np.arange(1,10).reshape(3,3)
-arr2 = np.arange(10,19).reshape(3,3)
-print(arr1,"and",arr2)
+# arr1 = np.arange(1,10).reshape(3,3)
+# arr2 = np.arange(10,19).reshape(3,3)
 
+array1=np.array([[0, 1, 3], [5, 7, 9]])
+array2=np.array([[0, 2, 4], [6, 8, 10]])
+print(array1)
+print("and")
+print(array2)
 # Concatenating operation
 # axis = 1 implies that it is being done column-wise
-print(np.concatenate((arr1,arr2),axis=1))
+print("Contacted array is ")
+print(np.concatenate((array1,array2),axis=1))
+ 
