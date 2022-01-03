1a3c363582fe7a5f4ad0383e4c0c9401ef7ca577
kanchanCode <jkanchan2911@gmail.com>
2022-01-03 10:43:25 +0530

    [Kanchan]Add:Added practice code

--- libraries_use/pandas_use.py
+++ libraries_use/pandas_use.py
@@ -27,7 +27,7 @@ ser=pd.Series(np.random.rand(34))
 print(ser)
 print(type(ser))
 
-newdf=pd.DataFrame(np.random).rand(100,5)
+newdf=pd.DataFrame(np.random.rand(100,5))
 print(newdf )
 print(newdf)
 
@@ -62,3 +62,72 @@ name.index=['first','second','third']
 print(name)
 #to fetch rows ->use index
 #to fetch column -? column name
+
+#DATAFRAME
+
+newdf1=pd.DataFrame(np.random.rand(334,5),index=np.arange(334))
+#pandas.DataFrame( data, index, columns, dtype, copy)
+
+print(newdf1)
+print(newdf.head(5))
+
+df=pd.DataFrame() 
+# empty dataframe
+
+data = [['Alex',10],['Bob',12],['Clarke',13]]
+df = pd.DataFrame(data,columns=['Name','Age'])
+print (df)
+#list to dataframe
+
+data={'name':['Abc','Xyz'],'age':[60,20]}
+df=pd.DataFrame(data,index=np.arange(2))
+print(df)
+print(type(df))
+#dict to dataframe
+
+print(newdf.describe())
+print(newdf.dtypes)
+
+print(newdf.index)
+print(newdf.columns)
+print(newdf.to_numpy())
+print(type(newdf.to_numpy()))
+#pandas t numpy
+
+print(newdf.T) #transpose
+print(newdf.sort_index(axis=0,ascending=False))  #sort rows descending
+
+print(newdf[0])
+print(type(newdf[0])) #series
+# combination of series->data frame
+
+#copy and view in pandas
+
+newdf2=newdf # it is a view for newdf
+newdf3=newdf.copy() # to copy
+
+newdf[0][0]=12346  # changing original data
+#newdf3[0][0]=8757
+
+print(newdf2) # view is getting reflected with the change
+print(newdf3) #no change 
+ 
+newdf.loc[0,0]=654  #  use loc to change the value
+print(newdf.head())
+print(newdf2.head())
+print(newdf3.head())
+
+newdf.columns=list("ABCDE") #clumn name change
+print(newdf.head(2))
+
+print(newdf.drop('D',axis=1)) # drop column
+
+print(newdf.loc[[1,2],['C','D']]) # returns a copy of it 
+print(newdf.loc[:,['C','D']]) # returns all row and C,D column
+print(newdf.loc[[1,2],:]) # returns all columns and 1,2 row
+
+
+#DATABASE TYPE QUERIES
+print(newdf.loc[newdf ['A']<0.3])
+
+print(newdf.loc[(newdf ['A']<0.3) & (newdf['C']>0.1)])
