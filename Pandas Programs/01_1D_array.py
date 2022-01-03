e17ddf7983f3dd4757d335faff0d37b50ed2ab65
kanchanCode <jkanchan2911@gmail.com>
2022-01-03 08:25:21 +0530

    [Kanchan]Add:Creating 1D array

---
+++ Pandas Programs/01_1D_array.py
@@ -0,0 +1,13 @@
+'''
+@Author: Kanchan
+@Date: 2022-10-01
+@Last Modified by: Kanchan
+@Last Modified time: 2022-01-01
+@Title :Write a Python program to create and display a one-dimensional array-like object
+containing an array of data using Pandas module. 
+'''
+
+import pandas as pd
+pd_ds=pd.Series([2,4,6,8,10])
+#series
+print(pd_ds)
