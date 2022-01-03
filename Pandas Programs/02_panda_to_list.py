3d7a41047a0e8a417e9871676cd80f067eae3c74
kanchanCode <jkanchan2911@gmail.com>
2022-01-03 08:36:15 +0530

    [Kanchan]Add:data series to list

---
+++ Pandas Programs/02_panda_to_list.py
@@ -0,0 +1,16 @@
+'''
+@Author: Kanchan
+@Date: 2022-10-01
+@Last Modified by: Kanchan
+@Last Modified time: 2022-01-01
+@Title :Write a Python program to convert a Panda module Series to Python list and it's type.
+'''
+
+import pandas as pd
+pd_ds=pd.Series([2,4,6,8,10])
+#series
+print(pd_ds)
+print(type(pd_ds))
+print("Converted list is")
+print(pd_ds.tolist())
+print(type(pd_ds.tolist()))
