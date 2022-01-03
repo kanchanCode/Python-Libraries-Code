a71002de8ca43a729cb65f018bf03a3aaac29bf9
kanchanCode <jkanchan2911@gmail.com>
2022-01-03 05:32:10 +0530

    [Kanchan]Add:scientific notation suppress

--- NumPy Programs/26_scientific_notation.py
+++ NumPy Programs/26_scientific_notation.py
@@ -5,7 +5,9 @@
 @Last Modified time: 2022-01-01
 @Title : Write a Python program to suppresses the use of scientific notation for small
 numbers in numpy array.
-
-
 '''
 import numpy as np
+np_arr=np.array([1.6e-10, 1.6, 1200, .235])
+print("Array is ",np_arr)
+np.set_printoptions(suppress=True)
+print(np_arr)
