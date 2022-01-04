0545feed34dfc631157c8f939625a3b8f34b2363
kanchanCode <jkanchan2911@gmail.com>
2022-01-04 09:47:00 +0530

    [Kanchan]Add:to print list of header

--- Pandas Programs/23_list_from_Data_frame.py
+++ Pandas Programs/23_list_from_Data_frame.py
@@ -15,4 +15,5 @@ exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Micha
              'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
 labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
 
-df=pd.DataFrame(exam_data,labels)
+df=pd.DataFrame(exam_data,index=labels)
+print(list(df.columns.values))
