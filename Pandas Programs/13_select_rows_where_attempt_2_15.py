8927bc1d5a0670eed1204bfdd56eaa91165d043a
kanchanCode <jkanchan2911@gmail.com>
2022-01-04 08:59:06 +0530

    [Kanchan]Add:select rows where no. of attempt<2 and score>15

--- Pandas Programs/13_select_rows_where_attempt_2_15.py
+++ Pandas Programs/13_select_rows_where_attempt_2_15.py
@@ -18,5 +18,5 @@ exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Micha
 labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
 
 df=pd.DataFrame(exam_data,labels)
-print("No. of attempts less than 2 and greater than 15 ")
+print("No. of attempts less than 2 and score is greater than 15 ")
 print(df[(df['attempts']<2)&(df['score']>15)])
