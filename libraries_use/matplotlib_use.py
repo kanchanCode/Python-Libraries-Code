4bbb82952642f68d92847617f7e4432557483ca3
kanchanCode <jkanchan2911@gmail.com>
2022-01-05 08:28:32 +0530

    [Kanchan]Add:Matplotlib use

---
+++ libraries_use/matplotlib_use.py
@@ -0,0 +1,27 @@
+
+# matplotlib
+'''
+1. Data VIsualizetion - It allow us to quickly interpret the data and adjust diff variables
+2.present data in a pictorial or graphical format
+-> Visulize-> Analyse -> Document Insight -> Transform Dataset->
+
+MATPLOTLIB is a pyhton package used for 2D 
+Types of PLOTS
+
+Bar Graph = chart or graph that presents categorical data with rectangular bars with heights or lengths proportional to the values
+To see difference in two groups (category variables)
+
+Histogrms= Cartesian coordinates to display values for typically two variables for a set of data.
+a graphical representation that organizes a group of data points into user-specified ranges (Quantative variables )
+
+Scatter Plot= to check how 2-3 variables relation(co-relation)
+
+Pie Plot =  Pie charts can be used to show percentages of a whole, and represents percentages at a set point in time.
+for percentage share of whole
+
+Area Plot = An area chart or area graph displays graphically quantitative data. It is based on the line chart. 
+similar to line graph  . to track changes in two or more groups that make up whole category
+
+Hexagonal Plot = A hexagonal bin plot is created by covering the data range with a regular array of hexagons and coloring each hexagon according to the number of observations it covers.
+
+'''
